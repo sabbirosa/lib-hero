2cb726c15db635a5ef31fc71bb9c6d842164e510
Sabbir Bin Abdul Latif <sabbir0727@gmail.com>
2025-06-22 13:31:45 +0600

    Refactor database connection by removing separate connectDB function and integrating it directly in server initialization

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import dotenv from "dotenv";
 import { Server } from "http";
+import mongoose from "mongoose";
 import app from "./app";
-import connectDB from "./app/config/database";
 
 dotenv.config();
 
 let server: Server;
 const port = process.env.PORT || 5000;
+const uri = process.env.MONGODB_URI;
 
 const bootstrap = async () => {
   try {
-    await connectDB();
+    await mongoose.connect(uri as string);
     console.log("Connected to MongoDB Using Mongoose!!");
     server = app.listen(port, () => {
       console.log(`LibHero Server is running on http://localhost:${port}`);
@@ -21,3 +22,25 @@ const bootstrap = async () => {
 };
 
 bootstrap();
+
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection at:", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
