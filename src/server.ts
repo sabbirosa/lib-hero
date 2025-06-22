b9f1193e7c3946b598a5c329f27899dfe9f3b02e
Sabbir Bin Abdul Latif <sabbir0727@gmail.com>
2025-06-22 13:23:02 +0600

    Refactor server initialization to connect to MongoDB before starting the server

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,18 @@
 import dotenv from "dotenv";
+import { Server } from "http";
 import app from "./app";
 import connectDB from "./app/config/database";
 
 dotenv.config();
 
+let server: Server;
 const port = process.env.PORT || 5000;
 
 const bootstrap = async () => {
   try {
-    app.listen(port, async () => {
-      await connectDB();
+    await connectDB();
+    console.log("Connected to MongoDB Using Mongoose!!");
+    server = app.listen(port, () => {
       console.log(`LibHero Server is running on http://localhost:${port}`);
     });
   } catch (err) {
